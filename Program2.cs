59eb102222ae9a7c4ba9754c8dac96e936c7361e
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-10 15:28:21 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,27 @@
+﻿class person
+{
+    static string officialAdress;
+    static readonly int lwp;
+
+
+    static person()
+    {
+    officialAdress="Lajpat Nagar";
+
+        lwp = 2;
+
+    }
+    public static void Main()
+    {
+        Console.WriteLine(officialAdress);
+        Console.WriteLine(lwp);
+
+       // lwp =4;
+        officialAdress = "Noida ";
+
+        Console.WriteLine(lwp + " " +officialAdress);
+
+    }
+
+
+}
