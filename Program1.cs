d00728b051d5063f0733a40740249479dcb00b67
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-10 15:27:06 +0530

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Variables
+{
+
+    class Variables
+    {
+
+        readonly int Id;
+        string Name;
+        double Salary;
+
+        // static string Address;
+        // static readonly string Policies;
+
+        const int Year= 2022;
+
+        public Variables(int id, string name, double sal)
+        {
+            this.Id = id;
+             this.Name = name;
+            this.Salary = sal;
+        }
+
+        public void display () {
+
+            Console.WriteLine("Employee id : " + Id + "\n" + "Employee Name :" + Name + "\n" + "Salary is : " + Salary + "\n" + "Year of Joining : " + Year);
+
+        }
+
+       public static void Main() {
+
+            Variables obj = new Variables(101, "Sadham Hussain", 10000);
+            obj.display();
+        
+        }
+     }
+}
